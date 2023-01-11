ef170af10babc648ba7550705bd3ff58db471df0
javiipzo <javipozo999@gmail.com>
2023-01-11 14:37:15 +0100

    yes

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+      'default': {
+          'ENGINE': 'django.db.backends.mysql',
+          'NAME': 'midatabase',
+          'USER': 'javipozo',
+          'PASSWORD': '12345',
+          'HOST': 'localhost',
+          'PORT': '',
+      }
+  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
